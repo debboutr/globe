c4d5dbdb1d3bdfce60ef283fd265c9796a75d6e8
debboutr <debboutr@gmail.com>
2023-01-20 22:54:45 -0800

    add tailwind, google fonts, split divs vertically

--- main.js
+++ main.js
@@ -4,19 +4,27 @@ import vertexShader from "./shaders/vertex.glsl"
 import fragmentShader from "./shaders/fragment.glsl"
 import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl"
 import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl"
+import { Float32BufferAttribute } from 'three';
 // Setup
+
+const canvasContainer = document.querySelector("#canvasContainer")
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    canvasContainer.offsetWidth/ canvasContainer.offsetHeight,
+    0.1,
+    1000);
 
 const renderer = new THREE.WebGLRenderer({
-    antialias: true
+    antialias: true,
+    canvas: document.querySelector('canvas')
 });
-renderer.setSize(innerWidth, innerHeight)
+
+renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
 
-document.body.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh( new THREE.SphereGeometry(5, 50, 50),
     new THREE.ShaderMaterial({
@@ -50,6 +58,23 @@ const group = new THREE.Group()
 group.add(sphere)
 scene.add(group)
 
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff
+})
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000
+    const y = (Math.random() - 0.5) * 2000
+    const z = -Math.random() * 2000
+    starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points( starGeometry, starMaterial )
+console.log(stars)
+scene.add(stars)
 //camera.position.x = -15
 //camera.position.y = 5
 camera.position.z = 25
